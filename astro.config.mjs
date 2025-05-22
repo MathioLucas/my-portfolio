8cadca0cd730ca2a1d3c870c3652d35833ffbc39
MathioLucas <162763121+MathioLucas@users.noreply.github.com>
2025-05-21 20:03:19 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/static'; // Make sure this matches what you installed
-
-// If the above import fails, try this alternative:
-// import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  output: 'static',
   adapter: vercel(),
   integrations: [tailwind()],
 });
